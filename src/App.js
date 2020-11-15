5ffe44d158a2dbeb060e483b54aca14945582515
Alex Calia <alex.m.calia@outlook.com>
2020-11-14 23:46:59 -0500

    axios lookups working, random categories and their questions are stored in state

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ const App = () => {
   useEffect(() => {
     // Get 6 random categories, save to an array
     const categories = [];
+    const questionSet = [];
     Axios.get(`https://jservice.io/api/categories?count=100`)
     .then(response => {
       for (let i = 1; i <= 6; i++) {
         categories.push(response.data.splice(Math.floor(Math.random() * response.data.length), 1)[0]);
       }
-      console.log(categories);
+    })
+    .then(() => {
+      categories.forEach(category => {
+        console.log(category.id)
+        Axios.get(`https://jservice.io/api/clues?category=${category.id}`)
+        .then(response => {
+          questionSet.push({
+            category: category.title,
+            questions: response.data
+          });
+          console.log(response.data);
+        });
+      });
     });
-  })
+
+    setQuestions(questionSet);
+  },[]);
 
   return (
     <div className="App">
