f02cae3eb554052e77b46135b97014a8e23e2057
Alex Calia <alex.m.calia@outlook.com>
2020-11-15 22:23:00 -0500

    created state for round two questions, expanded category API loop to 12

--- src/App.js
+++ src/App.js
@@ -3,33 +3,40 @@ import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 
 const App = () => {
-  const [questions, setQuestions] = useState([]);
+  const [questionsOne, setQuestionsOne] = useState([]);
+  const [questionsTwo, setQuestionsTwo] = useState([]);
 
   useEffect(() => {
     // Get 6 random categories, save to an array
     const categories = [];
-    const questionSet = [];
+    const questionSetOne = [];
+    const questionSetTwo = [];
     Axios.get(`https://jservice.io/api/categories?count=100`)
     .then(response => {
-      for (let i = 1; i <= 6; i++) {
+      for (let i = 1; i <= 12; i++) {
         categories.push(response.data.splice(Math.floor(Math.random() * response.data.length), 1)[0]);
       }
     })
     .then(() => {
       categories.forEach(category => {
-        console.log(category.id)
         Axios.get(`https://jservice.io/api/clues?category=${category.id}`)
         .then(response => {
-          questionSet.push({
-            category: category.title,
-            questions: response.data
-          });
-          console.log(response.data);
+          if (questionSetOne.length === 6) {
+            questionSetTwo.push({
+              category: category.title,
+              questions: response.data
+            });
+          } else {
+            questionSetOne.push({
+              category: category.title,
+              questions: response.data
+            });
+          };
         });
       });
     });
-
-    setQuestions(questionSet);
+    setQuestionsOne(questionSetOne);
+    setQuestionsTwo(questionSetTwo);
   },[]);
 
   return (
